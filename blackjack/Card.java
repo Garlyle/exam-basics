dfd3fd57aa72089d6d367048bbeec9fd7467589f
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-18 10:40:18 +0200

    create Card class

--- blackjack/Card.java
+++ blackjack/Card.java
@@ -1,2 +1,25 @@
 public class Card {
+  private final String[] COLORS = {"Club", "Heart", "Spade", "Diamond"};
+  private final String[] VALUES = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+  int color;
+  int value;
+
+  Card(int color, int value) {
+    this.color = color;
+    this.value = value;
+  }
+
+  public int possibleColors() {
+    return COLORS.length;
+  }
+
+  public int possibleValues() {
+    return VALUES.length;
+  }
+
+  @Override
+  public String toString() {
+    return VALUES[value] + " of " + COLORS[color];
+  }
 }
